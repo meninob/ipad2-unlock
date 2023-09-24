a72c36f15f1947016831723f3e16101492d86df4
Menino Borges <37333638+meninob@users.noreply.github.com>
2023-09-24 14:18:59 +0100

    Create inst_icu.sh

---
+++ inst_icu.sh
@@ -0,0 +1,9 @@
+apt update && apt install -y usbmuxd libimobiledevice6 libimobiledevice-utils libavahi-compat-libdnssd-dev curl wget procps usbutils lsof tmux build-essential pkg-config checkinstall git autoconf automake libtool-bin libplist-dev libimobiledevice-dev libxml2-dev libcurl4-openssl-dev usbmuxd
+git clone https://github.com/libimobiledevice/libideviceactivation.git
+cd libideviceactivation
+./autogen.sh
+make
+make install
+cp tools/ideviceactivation/*.* /bin
+ldconfig -v |& grep mobile
+echo 'ls /bin >> /home/lsbin.txt'
