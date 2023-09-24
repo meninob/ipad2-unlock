34e16c1a37a91fd6a8cce7fc3cea527401c5c524
Menino Borges <37333638+meninob@users.noreply.github.com>
2023-09-24 14:26:37 +0100

    Update inst_icu.sh

--- inst_icu.sh
+++ inst_icu.sh
@@ -1,10 +1,11 @@
+cd /home
 apt update && apt install -y usbmuxd libimobiledevice6 libimobiledevice-utils libavahi-compat-libdnssd-dev curl wget procps usbutils lsof tmux build-essential pkg-config checkinstall git autoconf automake libtool-bin libplist-dev libimobiledevice-dev libxml2-dev libcurl4-openssl-dev usbmuxd
 git clone https://github.com/libimobiledevice/libideviceactivation.git
 cd libideviceactivation
 ./autogen.sh
 make
 make install
-cp libideviceactivation/tools/ideviceactivation/*.* /bin/
+cp /home/libideviceactivation/tools/ideviceactivation/*.* /bin/
 ldconfig -v |& grep mobile
 echo 'ls /bin >> /home/lsbin.txt'
 ideviceactivation activate -s cengdealajr.gearhostpreview.com/sliver.php -d
