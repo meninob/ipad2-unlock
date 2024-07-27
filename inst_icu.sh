5c5682f1b4f266449240dd74495d38bd25cca3f4
Menino Borges <37333638+meninob@users.noreply.github.com>
2024-07-27 06:34:59 +0100

    Update inst_icu.sh

--- inst_icu.sh
+++ inst_icu.sh
@@ -1,8 +1,9 @@
 !/bin/bash
 cd /home
-apt update && apt install -y usbmuxd libimobiledevice6 libimobiledevice-utils libavahi-compat-libdnssd-dev curl wget procps usbutils lsof tmux build-essential \
+apt update && apt install -y usbmuxd libimobiledevice-1.0-6 libimobiledevice-utils libavahi-compat-libdnssd-dev curl wget procps usbutils lsof tmux build-essential \
   pkg-config git libtool nodejs libpq-dev usbutils usbmuxd make automake autoconf gcc doxygen libusbmuxd-dev libplist-dev libplist++-dev libssl-dev \
-  checkinstall libusb-1.0-0-dev 
+  checkinstall libusb-1.0-0-dev
+#ibimobiledevice6
 git clone https://github.com/libimobiledevice/libideviceactivation.git
 cd libideviceactivation
 ./autogen.sh
