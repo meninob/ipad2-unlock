5ac1f2dc949ad15dfa044a5bb192e94e05ce8fac
Menino Borges <37333638+meninob@users.noreply.github.com>
2024-07-27 07:14:25 +0100

    Update inst_icu.sh

--- inst_icu.sh
+++ inst_icu.sh
@@ -1,17 +1,36 @@
 !/bin/bash
 cd /home
-apt update && apt install -y usbmuxd libimobiledevice-1.0-6 libimobiledevice-utils libavahi-compat-libdnssd-dev curl wget procps usbutils lsof tmux build-essential \
+apt update && apt install -y usbmuxd libtool-bin libimobiledevice-utils libavahi-compat-libdnssd-dev curl wget procps usbutils lsof tmux build-essential \
   pkg-config git libtool nodejs libpq-dev usbutils usbmuxd make automake autoconf gcc doxygen libusbmuxd-dev libplist-dev libplist++-dev libssl-dev \
-  checkinstall libusb-1.0-0-dev
-#ibimobiledevice6
+  checkinstall libusb-1.0-0-dev libtatsu
+#libimobiledevice-glue-1.0
+#libimobiledevice6 libimobiledevice-1.0-6
+export PKG_CONFIG_PATH=/usr/loca;/lib/pkgconfig 
+
+git clone https://github.com/libimobiledevice/libimobiledevice-glue
+cd libimobiledevice-glue
+./autogen.sh
+make 
+make install
+cd ..
+
+git clone https://github.com/libimobiledevice/libimobiledevice.git
+cd libimobiledevice
+./autogen.sh
+make
+make install
+cd ..
+ldconfig -v |& grep mobile
 git clone https://github.com/libimobiledevice/libideviceactivation.git
 cd libideviceactivation
-./autogen.sh
+PKG_CONFIG_PATH=/etc/pkgconfig ./autogen.sh
 make
 make install
 #cp /home/libideviceactivation/tools/ideviceactivation/*.* /bin/
 ldconfig -v |& grep mobile
 echo 'ls /bin >> /home/lsbin.txt'
+cd ..
 #usbmuxd -v
 #/home/unicloud.sh
+
 #ideviceactivation activate -s cengdealajr.gearhostpreview.com/sliver.php -d
